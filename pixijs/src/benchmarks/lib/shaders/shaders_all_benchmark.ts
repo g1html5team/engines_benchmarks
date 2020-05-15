60f901502a47817daa960ad5917d4ef96e15f7d0
Simon Chupin <simon.chupin@gaming1.com>
2020-05-15 14:23:33 +0200

    feat: Implement shaders step by step benchmark

--- pixijs/src/benchmarks/lib/shaders/shaders_all_benchmark.ts
+++ pixijs/src/benchmarks/lib/shaders/shaders_all_benchmark.ts
@@ -1,36 +1,19 @@
 import * as PIXI from 'pixi.js';
-import { Benchmark } from '../benchmark';
 import { ShaderType } from './shader_type';
+import { AShadersBenchmark } from './a_shaders_benchmark';
 
-export class ShadersAllBenchmark extends Benchmark {
-  private movingBunny: PIXI.Sprite;
-  private movingBunnyDirection = new PIXI.Point(1, 0.5);
-
-  public constructor(width: number, height: number, private numberOfObjects: number) {
-    super(width, height);
-    this.assetsToLoad.set('bunny', '../assets/bunny.png');
-    this.assetsToLoad.set('background_fields', '../assets/background_fields.jpg');
+export class ShadersAllBenchmark extends AShadersBenchmark {
+  public constructor(width: number, height: number, numberOfObjects: number) {
+    super(width, height, numberOfObjects, true);
   }
 
   public async init(): Promise<void> {
     await super.init();
 
-    const container = new PIXI.Container();
-
-    const background = new PIXI.Sprite(this.app.loader.resources.background_fields.texture);
-    this.app.stage.addChild(background);
-
-    this.movingBunny = new PIXI.Sprite(this.app.loader.resources.bunny.texture);
-    container.addChild(this.movingBunny);
-    this.movingBunny.anchor = new PIXI.Point(0.5, 0.5);
-    this.movingBunny.alpha = 0.25;
-    this.movingBunny.x = 25;
-    this.movingBunny.y = 75;
-
     let shaderType = 0;
     for (let i = 0; i < this.numberOfObjects; i++) {
       const bunny = new PIXI.Sprite(this.app.loader.resources.bunny.texture);
-      container.addChild(bunny);
+      this.container.addChild(bunny);
       bunny.x = 50 + Math.random() * (this.app.renderer.screen.width - 50);
       bunny.y = 50 + Math.random() * (this.app.renderer.screen.height - 50);
       bunny.rotation = Math.random() * Math.PI;
@@ -51,23 +34,5 @@ export class ShadersAllBenchmark extends Benchmark {
 
       shaderType = (shaderType + 1) % 3;
     }
-
-    this.app.stage.addChild(container);
-  }
-
-  protected eachFrameCallback(): void {
-    if (this.movingBunny.x > this.app.renderer.screen.width) {
-      this.movingBunnyDirection.set(-1 * Math.random(), this.movingBunnyDirection.y);
-    } else if (this.movingBunny.x < 0) {
-      this.movingBunnyDirection.set(Math.random(), this.movingBunnyDirection.y);
-    }
-    if (this.movingBunny.y > this.app.renderer.screen.height) {
-      this.movingBunnyDirection.set(this.movingBunnyDirection.x * Math.random(), -1 * Math.random());
-    } else if (this.movingBunny.y < 0) {
-      this.movingBunnyDirection.set(this.movingBunnyDirection.x * Math.random(), Math.random());
-    }
-
-    this.movingBunny.x += this.movingBunnyDirection.x * 100;
-    this.movingBunny.y += this.movingBunnyDirection.y * 100;
   }
 }
--- pixijs/src/benchmarks/lib/shaders/shaders_step_by_step_benchmark.ts
+++ pixijs/src/benchmarks/lib/shaders/shaders_step_by_step_benchmark.ts
@@ -1,48 +1,54 @@
 import * as PIXI from 'pixi.js';
-import { Benchmark } from '../benchmark';
+import { ShaderType } from './shader_type';
+import { AShadersBenchmark } from './a_shaders_benchmark';
 
-export class ShadersStepByStepBenchmark extends Benchmark {
-  private movingBunny: PIXI.Sprite;
-  private movingBunnyDirection = new PIXI.Point(1, 0.5);
+export class ShadersStepByStepBenchmark extends AShadersBenchmark {
+  private bunnies: PIXI.Sprite[] = [];
+  private currentShaderType = ShaderType.Blur;
+  private nextStepTime = 750;
 
-  public constructor(width: number, height: number, private numberOfObjects: number) {
-    super(width, height);
-    this.assetsToLoad.set('bunny', '../assets/bunny.png');
-    this.assetsToLoad.set('background_fields', '../assets/background_fields.jpg');
+  public constructor(width: number, height: number, numberOfObjects: number) {
+    super(width, height, numberOfObjects, true);
   }
 
-  public async init(): Promise<void> {
-    await super.init();
+  protected eachFrameCallback(deltaTime: number, timeSinceStart: number): void {
+    super.eachFrameCallback();
 
-    const container = new PIXI.Container();
+    if (timeSinceStart > this.nextStepTime) {
+      this.nextStepTime += 750;
 
-    const background = new PIXI.Sprite(this.app.loader.resources.background_fields.texture);
-    this.app.stage.addChild(background);
+      if (this.bunnies.length >= this.numberOfObjects) {
+        this.bunnies.forEach((bunny) => {
+          this.container.removeChild(bunny);
+        });
 
-    this.movingBunny = new PIXI.Sprite(this.app.loader.resources.bunny.texture);
-    container.addChild(this.movingBunny);
-    this.movingBunny.anchor = new PIXI.Point(0.5, 0.5);
-    this.movingBunny.alpha = 0.25;
-    this.movingBunny.x = 25;
-    this.movingBunny.y = 75;
+        this.bunnies = [];
+      }
 
-    this.app.stage.addChild(container);
-  }
+      for (let i = 0; i < this.numberOfObjects / 3; i++) {
+        const bunny = new PIXI.Sprite(this.app.loader.resources.bunny.texture);
+        this.container.addChild(bunny);
+        bunny.x = 50 + Math.random() * (this.app.renderer.screen.width - 50);
+        bunny.y = 50 + Math.random() * (this.app.renderer.screen.height - 50);
+        bunny.rotation = Math.random() * Math.PI;
+        bunny.anchor = new PIXI.Point(0.5, 0.5);
 
-  protected eachFrameCallback(): void {
-    // TODO (sch) don't understand former benchmark logic and it doesn't really work
-    if (this.movingBunny.x > this.app.renderer.screen.width) {
-      this.movingBunnyDirection.set(-1 * Math.random(), this.movingBunnyDirection.y);
-    } else if (this.movingBunny.x < 0) {
-      this.movingBunnyDirection.set(Math.random(), this.movingBunnyDirection.y);
+        let filter: PIXI.Filter;
+        switch (this.currentShaderType) {
+          case ShaderType.Blur:
+            filter = new PIXI.filters.BlurFilter();
+            this.currentShaderType = ShaderType.Glow;
+            break;
+          case ShaderType.Displacement:
+            filter = new PIXI.filters.DisplacementFilter(this.movingBunny, 5.0);
+            this.currentShaderType = ShaderType.Blur;
+            break;
+          default:
+            filter = new PIXI.filters.NoiseFilter();
+            this.currentShaderType = ShaderType.Displacement;
+        }
+        bunny.filters = [filter];
+      }
     }
-    if (this.movingBunny.y > this.app.renderer.screen.height) {
-      this.movingBunnyDirection.set(this.movingBunnyDirection.x * Math.random(), -1 * Math.random());
-    } else if (this.movingBunny.y < 0) {
-      this.movingBunnyDirection.set(this.movingBunnyDirection.x * Math.random(), Math.random());
-    }
-
-    this.movingBunny.x += this.movingBunnyDirection.x * 100;
-    this.movingBunny.y += this.movingBunnyDirection.y * 100;
   }
 }
