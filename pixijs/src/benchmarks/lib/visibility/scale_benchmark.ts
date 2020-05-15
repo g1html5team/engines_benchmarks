7b1e6d3cee499b6019f60230ef113b6841631580
Simon Chupin <simon.chupin@gaming1.com>
2020-05-15 13:37:56 +0200

    fix: Adjust timeSinceStart parameter

--- pixijs/src/benchmarks/lib/visibility/scale_benchmark.ts
+++ pixijs/src/benchmarks/lib/visibility/scale_benchmark.ts
@@ -3,8 +3,8 @@ import { AVisibilityBenchmark } from './a_visibility_benchmark';
 
 export class ScaleBenchmark extends AVisibilityBenchmark {
   // eslint-disable-next-line class-methods-use-this
-  protected eachFrameCallback(deltaTime: number, lastTime: number): void {
-    if (lastTime > 2000) {
+  protected eachFrameCallback(deltaTime: number, timeSinceStart: number): void {
+    if (timeSinceStart > 2000) {
       this.bunnies.forEach((bunny) => {
         bunny.x = Math.random() * this.app.renderer.screen.width;
         bunny.y = Math.random() * this.app.renderer.screen.height;
