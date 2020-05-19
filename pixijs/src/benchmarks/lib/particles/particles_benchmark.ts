bea96f325b6fc4b68412fe913d2eac922f699b25
Simon Chupin <simon.chupin@gaming1.com>
2020-05-19 18:44:51 +0200

    feat: Implement particles

--- pixijs/src/benchmarks/lib/particles/particles_benchmark.ts
+++ pixijs/src/benchmarks/lib/particles/particles_benchmark.ts
@@ -1,48 +1,115 @@
 import * as PIXI from 'pixi.js';
+import * as particles from 'pixi-particles';
 import { Benchmark } from '../benchmark';
 
 export class ParticlesBenchmark extends Benchmark {
-  private particles: PIXI.Container[] = [];
+  private container = new PIXI.Container();
+  private emitter: particles.Emitter;
 
   public constructor(width: number, height: number, private numberOfObjects: number) {
     super(width, height);
-    this.assetsToLoad.set('bunny', '../assets/bunny.png');
+    this.assetsToLoad.set('particle', '../assets/particles/particle.png');
   }
 
   public async init(): Promise<void> {
     await super.init();
 
-    const container = new PIXI.Container();
+    this.container.x = Math.random() * this.app.renderer.screen.width;
+    this.container.y = Math.random() * this.app.renderer.screen.height;
 
-    for (let i = 0; i < this.numberOfObjects; i++) {
-      const particle = ParticlesBenchmark.createParticle();
-      container.addChild(particle);
-      particle.x = Math.random() * this.app.renderer.screen.width;
-      particle.y = Math.random() * this.app.renderer.screen.height;
-      this.particles.push(particle);
-    }
-
-    this.app.stage.addChild(container);
-  }
-
-  private static createParticle(): PIXI.Container {
-    const radius = 20;
-    const container = new PIXI.Container();
-    const points = new PIXI.Graphics();
-
-    points.beginFill(0xffff0b, 1);
-    for (let i = -20; i <= 20; i++) {
-      points.drawCircle(i, Math.sqrt(radius * radius - i * i), 2);
-    }
-    points.endFill();
-    container.addChild(points);
-
-    return container;
-  }
-
-  protected eachFrameCallback(): void {
-    this.particles.forEach(() => {
-      // TODO (sch)
+    this.emitter = new particles.Emitter(this.container, [this.app.loader.resources.particle.texture], {
+      alpha: {
+        list: [
+          {
+            value: 1,
+            time: 0,
+          },
+          {
+            value: 0,
+            time: 1,
+          },
+        ],
+        isStepped: false,
+      },
+      scale: {
+        list: [
+          {
+            value: 1,
+            time: 0,
+          },
+          {
+            value: 2,
+            time: 1,
+          },
+        ],
+        isStepped: false,
+      },
+      color: {
+        list: [
+          {
+            value: 'fb1010',
+            time: 0,
+          },
+          {
+            value: 'f5b830',
+            time: 1,
+          },
+        ],
+        isStepped: false,
+      },
+      speed: {
+        list: [
+          {
+            value: 200,
+            time: 0,
+          },
+          {
+            value: 100,
+            time: 1,
+          },
+        ],
+        isStepped: false,
+      },
+      startRotation: {
+        min: 0,
+        max: 360,
+      },
+      rotationSpeed: {
+        min: 0,
+        max: 0,
+      },
+      rotationAcceleration: 20,
+      lifetime: {
+        min: 0.5,
+        max: 0.9,
+      },
+      acceleration: {
+        x: 10,
+        y: 10,
+      },
+      frequency: 0.008,
+      spawnChance: 1,
+      particlesPerWave: 1,
+      emitterLifetime: -1,
+      maxParticles: 1000,
+      pos: {
+        x: 0,
+        y: 0,
+      },
+      addAtBack: false,
+      spawnType: 'circle',
+      spawnCircle: {
+        x: 0,
+        y: 0,
+        r: 5,
+      },
+      autoUpdate: true,
     });
+    this.emitter.emit = true;
+
+    this.app.stage.addChild(this.container);
   }
+
+  // eslint-disable-next-line class-methods-use-this
+  protected eachFrameCallback(): void {}
 }
